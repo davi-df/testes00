00e736e915b14047b86b0fb6fea02e3494dd344b
davi-df <davidantasf113@gmail.com>
2026-07-26 15:06:54 -0300

    Adicionando primeira versão do projeto.

---
+++ calculos.js
@@ -0,0 +1,19 @@
+
+function calcular() {
+    //__________BUSCANDO O VALOR DADO AOS INPUTS A, B e C_____________
+    let a = document.getElementById("inptA").value;
+    let b = document.getElementById("inptB").value;
+    let c = document.getElementById("inptC").value;
+
+    //__________________ÁREA DE CÁLCULOS______________________________
+    const delta = (b**2) - (4 * a * c);
+    const bhaskara = (-b + (Math.sqrt(delta))) / (2 * a);
+    const bhaskara1 = (-b - (Math.sqrt(delta))) / (2 * a);
+    // Imprime na div vazia o resultado   
+    let mostra = document.getElementById("res").innerHTML += `DELTA É: ${delta}<br> BHASKARA x¹ É ${bhaskara}<br> BHASKARA x² É ${bhaskara1}` ;
+
+    //__ZERANDO OS VALORES DIGITADOS NOS INPUTS_______________________
+    document.getElementById("inptA").value = 0;
+    document.getElementById("inptB").value = 0;
+    document.getElementById("inptC").value = 0;
+    }
